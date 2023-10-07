1b80cda808bacf003e6f6c7d6d60847567d83e73
dagiprogrammer <dagiprogrammer@gmail.com>
2023-10-07 14:13:20 -0600

    navbar responsiveness

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+//ham menu
+const hamBtn = document.getElementById('ham-btn');
+const hamMenu = document.getElementById('main-menu');
+
+hamBtn.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    hamBtn.classList.toggle('active');
+})
+
+alert('hey')
